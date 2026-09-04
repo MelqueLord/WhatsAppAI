29cbc608714308db59bd6189bca8f256306eda5d
MelqueLord <jmelquesantos@gmail.com>
2026-09-04 08:43:10 -0300

    fix(persistence): include queue notice migration metadata

--- src/WhatsAppAI.Infrastructure/Migrations/20260904000000_AddServiceQueueTransferNotice.cs
+++ src/WhatsAppAI.Infrastructure/Migrations/20260904000000_AddServiceQueueTransferNotice.cs
@@ -1,13 +1,9 @@
-using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
-using WhatsAppAI.Infrastructure.Persistence;
 
 #nullable disable
 
 namespace WhatsAppAI.Infrastructure.Migrations;
 
-[DbContext(typeof(AppDbContext))]
-[Migration("20260904000000_AddServiceQueueTransferNotice")]
 public partial class AddServiceQueueTransferNotice : Migration
 {
     protected override void Up(MigrationBuilder migrationBuilder)
